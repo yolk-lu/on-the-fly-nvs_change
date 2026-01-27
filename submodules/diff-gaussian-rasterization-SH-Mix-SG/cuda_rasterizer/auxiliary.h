0b3b0583514f8aa0ddd39a789484ba6f9bb81310
yolk_lu <113753132@g.nccu.edu.tw>
2026-01-28 01:39:01 +0800

    Refactor: Move SH/SG helpers to auxiliary.h, fix unused variables, and ensure proper SG-to-SH analytic projection

--- submodules/diff-gaussian-rasterization-SH-Mix-SG/cuda_rasterizer/auxiliary.h
+++ submodules/diff-gaussian-rasterization-SH-Mix-SG/cuda_rasterizer/auxiliary.h
@@ -14,10 +14,12 @@
 
 #include "config.h"
 #include "stdio.h"
+#include <glm/glm.hpp>
 
 #define BLOCK_SIZE (BLOCK_X * BLOCK_Y)
 #define NUM_WARPS (BLOCK_SIZE/32)
 #define DGR_FIX_AA
+
 // Spherical harmonics coefficients
 __device__ const float SH_C0 = 0.28209479177387814f;
 __device__ const float SH_C1 = 0.4886025119029199f;
@@ -166,7 +168,7 @@ __forceinline__ __device__ bool in_frustum(int idx,
 	// Bring points to screen space
 	float4 p_hom = transformPoint4x4(p_orig, projmatrix);
 	float p_w = 1.0f / (p_hom.w + 0.0000001f);
-	float3 p_proj = { p_hom.x * p_w, p_hom.y * p_w, p_hom.z * p_w };
+	// float3 p_proj = { p_hom.x * p_w, p_hom.y * p_w, p_hom.z * p_w };
 	p_view = transformPoint4x3(p_orig, viewmatrix); // rot
 
 	if (p_view.z <= 0.2f)// || ((p_proj.x < -1.3 || p_proj.x > 1.3 || p_proj.y < -1.3 || p_proj.y > 1.3)))
@@ -181,6 +183,118 @@ __forceinline__ __device__ bool in_frustum(int idx,
 	return true;
 }
 
+// --------------------------------------------------------------------------
+// SH / SG Helper functions (Moved from forward.cu / backward.cu)
+// --------------------------------------------------------------------------
+
+// Evaluate SH basis functions Y_lm(dir) for a given direction.
+// Stores results in the provided output array (up to degree 3, 16 coeffs).
+__forceinline__ __device__ void evalSHBasis(const glm::vec3 dir, float* result)
+{
+	result[0] = 0.28209479177387814f; // Y_00
+
+	float x = dir.x;
+	float y = dir.y;
+	float z = dir.z;
+
+	result[1] = -0.4886025119029199f * y;
+	result[2] = 0.4886025119029199f * z;
+	result[3] = -0.4886025119029199f * x;
+
+	float xx = x * x, yy = y * y, zz = z * z;
+	float xy = x * y, yz = y * z, xz = x * z;
+
+	result[4] = 1.0925484305920792f * xy;
+	result[5] = -1.0925484305920792f * yz;
+	result[6] = 0.31539156525252005f * (2.0f * zz - xx - yy);
+	result[7] = -1.0925484305920792f * xz;
+	result[8] = 0.5462742152960396f * (xx - yy);
+
+	result[9] = -0.5900435899266435f * y * (3.0f * xx - yy);
+	result[10] = 2.890611442640554f * xy * z;
+	result[11] = -0.4570457994644658f * y * (4.0f * zz - xx - yy);
+	result[12] = 0.3731763325901154f * z * (2.0f * zz - 3.0f * xx - 3.0f * yy);
+	result[13] = -0.4570457994644658f * x * (4.0f * zz - xx - yy);
+	result[14] = 1.445305721320277f * z * (xx - yy);
+	result[15] = -0.5900435899266435f * x * (xx - 3.0f * yy);
+}
+
+// Evaluate gradients of SH basis functions Y_lm(dir) w.r.t direction.
+// Result stored as 16 vec3s.
+__forceinline__ __device__ void dEvalSHBasis(const glm::vec3 dir, glm::vec3* results)
+{
+	// Y00 const, grad 0
+	results[0] = {0.f, 0.f, 0.f};
+
+	// Y1m
+	// Y1-1 = -C1*y, Y10 = C1*z, Y11 = -C1*x
+	// grad: (0, -C1, 0), (0, 0, C1), (-C1, 0, 0)
+	float C1 = 0.4886025119029199f;
+	results[1] = {0.f, -C1, 0.f};
+	results[2] = {0.f, 0.f, C1};
+	results[3] = {-C1, 0.f, 0.f};
+
+	float x = dir.x;
+	float y = dir.y;
+	float z = dir.z;
+
+	// Band 2
+	float C2_0 = 1.0925484305920792f;
+	float C2_1 = 1.0925484305920792f;
+	float C2_2 = 0.31539156525252005f;
+	float C2_3 = 1.0925484305920792f;
+	float C2_4 = 0.5462742152960396f;
+
+	results[4] = {C2_0 * y, C2_0 * x, 0.f}; // xy
+	results[5] = {0.f, -C2_1 * z, -C2_1 * y}; // yz
+	results[6] = {C2_2 * -2.f * x, C2_2 * -2.f * y, C2_2 * 4.f * z}; 
+	results[7] = {-C2_3 * z, 0.f, -C2_3 * x}; // xz
+	results[8] = {C2_4 * 2.f * x, -C2_4 * 2.f * y, 0.f}; // xx-yy
+
+	// Band 3
+	float C3_0 = 0.5900435899266435f;
+	float C3_1 = 2.890611442640554f;
+	float C3_2 = 0.4570457994644658f;
+	float C3_3 = 0.3731763325901154f;
+	float C3_4 = 0.4570457994644658f;
+	float C3_5 = 1.445305721320277f;
+	float C3_6 = 0.5900435899266435f;
+
+	float xx = x * x, yy = y * y, zz = z * z;
+	float xy = x * y, yz = y * z, xz = x * z;
+
+	results[9] = {-C3_0 * 6.f * xy, -C3_0 * 3.f * (xx - yy), 0.f};
+	results[10] = {C3_1 * yz, C3_1 * xz, C3_1 * xy};
+	results[11] = {2.f * C3_2 * xy, -C3_2 * (4.f * zz - xx - 3.f * yy), -8.f * C3_2 * yz};
+	results[12] = {-6.f * C3_3 * xz, -6.f * C3_3 * yz, C3_3 * (6.f * zz - 3.f * xx - 3.f * yy)};
+	results[13] = {-C3_4 * (4.f * zz - 3.f * xx - yy), 2.f * C3_4 * xy, -8.f * C3_4 * xz};
+	results[14] = {2.f * C3_5 * xz, -2.f * C3_5 * yz, C3_5 * (xx - yy)};
+	results[15] = {-C3_6 * 3.f * (xx - yy), 6.f * C3_6 * xy, 0.f};
+}
+
+// Evaluate Zonal Harmonic (ZH) coefficient g_{i,l} Approx
+__forceinline__ __device__ float evalZHApprox(int l, float lambda)
+{
+	if (l == 0) return 1.0f - exp(-2.0f * lambda);
+	return exp(-(float)(l * (l + 1)) / (2.0f * lambda));
+}
+
+__forceinline__ __device__ float evalZHApproxVal(int l, float lambda)
+{
+    if (l == 0) return 1.0f - exp(-2.0f * lambda);
+    return exp(-(float)(l * (l + 1)) / (2.0f * lambda));
+}
+
+__forceinline__ __device__ float evalZHApproxGrad(int l, float lambda)
+{
+	if (l == 0) return 2.0f * exp(-2.0f * lambda);
+	
+	float num = (float)(l * (l + 1));
+	float exponent = -num / (2.0f * lambda);
+	float g = exp(exponent);
+	return g * (num / (2.0f * lambda * lambda));
+}
+
 #define CHECK_CUDA(A, debug) \
 A; if(debug) { \
 auto ret = cudaDeviceSynchronize(); \
@@ -188,6 +302,95 @@ if (ret != cudaSuccess) { \
 std::cerr << "\n[CUDA ERROR] in " << __FILE__ << "\nLine " << __LINE__ << ": " << cudaGetErrorString(ret); \
 throw std::runtime_error(cudaGetErrorString(ret)); \
 } \
+}
+
+// Convert Spherical Gaussian (SG) parameters to SH coefficients using Analytic Projection.
+// f_lm = Sum_i [ Y_lm(xi_i) * sqrt(4pi/(2l+1)) * g_{i,l} * w_i ]
+// Max supported degree 3 (16 coeffs).
+__forceinline__ __device__ void computeSHFromSG(int idx, int max_coeffs_sg, const float* shs, float* result_sh_coeffs)
+{
+	// Initialize SH coeffs to 0
+	for (int i = 0; i < 16 * 3; ++i) result_sh_coeffs[i] = 0.0f;
+
+	// shs points to the start of SG params for this Gaussian
+	// Layout per lobe (7 floats):
+	// [0-2]: Amplitude (RGB)
+	// [3-5]: Axis (XYZ)
+	// [6]: Sharpness (Scalar)
+	const float* params = shs + idx * max_coeffs_sg;
+	
+	// Determine number of lobes based on stride 7
+	// If M is not multiple of 7, this implies error or extra padding, but we assume packed 7.
+	int num_lobes = max_coeffs_sg / 7;
+
+	// Temporary storage for SH basis evaluated at lobe axis
+	float basis[16];
+
+	// Constants for Normalization: sqrt(4pi / (2l+1))
+	const float C_norm[4] = {
+		3.544907701811032f,  // l=0: sqrt(4pi)
+		2.046653415892977f,  // l=1: sqrt(4pi/3)
+		1.585330919042404f,  // l=2: sqrt(4pi/5)
+		1.354055224345298f   // l=3: sqrt(4pi/7)
+	};
+
+	for (int k = 0; k < num_lobes; ++k)
+	{
+		// Offset for current lobe
+		const float* lobe_params = params + k * 7;
+		
+		glm::vec3 amplitude = {lobe_params[0], lobe_params[1], lobe_params[2]};
+		glm::vec3 axis = {lobe_params[3], lobe_params[4], lobe_params[5]};
+		float sharpness = lobe_params[6];
+
+		// Normalize axis
+		float axis_len = glm::length(axis);
+		if (axis_len > 1e-6f)
+			axis = axis / axis_len;
+		
+		// Evaluate SH basis at lobe axis: Y_lm(xi)
+		evalSHBasis(axis, basis);
+
+		// Sharpness lambda (stored as log scale in sharpness?) 
+		// User code in scene_model just inits 7 dim tensor. 
+		// Usually 3DGS stores "activation" applied later. 
+		// Standard: exp(param) to ensure pos. 
+		float lambda = exp(sharpness);
+
+		// Band 0 (l=0)
+		float g0 = evalZHApprox(0, lambda);
+		float scale0 = C_norm[0] * g0;
+		result_sh_coeffs[0] += amplitude.x * scale0 * basis[0];
+		result_sh_coeffs[16] += amplitude.y * scale0 * basis[0];
+		result_sh_coeffs[32] += amplitude.z * scale0 * basis[0];
+
+		// Band 1 (l=1)
+		float g1 = evalZHApprox(1, lambda);
+		float scale1 = C_norm[1] * g1;
+		for (int i = 1; i <= 3; ++i) {
+			result_sh_coeffs[i] += amplitude.x * scale1 * basis[i];
+			result_sh_coeffs[16 + i] += amplitude.y * scale1 * basis[i];
+			result_sh_coeffs[32 + i] += amplitude.z * scale1 * basis[i];
+		}
+
+		// Band 2 (l=2)
+		float g2 = evalZHApprox(2, lambda);
+		float scale2 = C_norm[2] * g2;
+		for (int i = 4; i <= 8; ++i) {
+			result_sh_coeffs[i] += amplitude.x * scale2 * basis[i];
+			result_sh_coeffs[16 + i] += amplitude.y * scale2 * basis[i];
+			result_sh_coeffs[32 + i] += amplitude.z * scale2 * basis[i];
+		}
+
+		// Band 3 (l=3)
+		float g3 = evalZHApprox(3, lambda);
+		float scale3 = C_norm[3] * g3;
+		for (int i = 9; i <= 15; ++i) {
+			result_sh_coeffs[i] += amplitude.x * scale3 * basis[i];
+			result_sh_coeffs[16 + i] += amplitude.y * scale3 * basis[i];
+			result_sh_coeffs[32 + i] += amplitude.z * scale3 * basis[i];
+		}
+	}
 }
 
 #endif
