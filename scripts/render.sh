02500fab30534b5964e5b9b617636a9324e1b1ce
yolk_lu <113753132@g.nccu.edu.tw>
2026-01-30 04:29:59 +0800

    Update submodule to fix SG gradient shape and memory access; Update scripts

--- scripts/render.sh
+++ scripts/render.sh
@@ -41,13 +41,25 @@
 #     --tau_min 0.8 --tau_max 2.0 \
 #     --k_ecc 0.3
 
+
+
 # Example: Dynamic LoD with 2x Zoom (Detail should appear in center)
+# python scripts/render_smooth_lod.py \
+#    -m ./results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5/lod_3 \
+#    --lod_fine 3 \
+#    --lod_coarse 1 \
+#    --render_path ./results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5/colmap \
+#    --out_dir ./results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5/lod_3/render_zoom2x \
+#    --tau_min 0.8 --tau_max 2.0 \
+#    --k_ecc 0.3 \
+#    --zoom_mult 2.0
+
+# diff-gaussian-rasterizer-SH-Mix-SG test
 python scripts/render_smooth_lod.py \
-   -m ./results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5/lod_3 \
+   -m ./results/Mip-NeRF360/garden/changing_LoD_v1/diff_gaussian_rasterizer_SH_MIX_SG_downsample3.5/lod_3 \
    --lod_fine 3 \
    --lod_coarse 1 \
-   --render_path ./results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5/colmap \
-   --out_dir ./results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5/lod_3/render_zoom2x \
+   --render_path ./results/Mip-NeRF360/garden/changing_LoD_v1/diff_gaussian_rasterizer_SH_MIX_SG_downsample3.5/colmap \
+   --out_dir ./results/Mip-NeRF360/garden/changing_LoD_v1/diff_gaussian_rasterizer_SH_MIX_SG_downsample3.5/render_SH_MIX_SG \
    --tau_min 0.8 --tau_max 2.0 \
    --k_ecc 0.3 \
-   --zoom_mult 2.0
--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@
 
 # python train.py -s /home/cglab/project/Dataset_opensource/Mip-NeRF360/garden -m results/Mip-NeRF360/garden/downsample_4 --downsampling 4 --test_hold 20 --lod_min 1 --lod_max 3 #--increase_lod_num_childs 2
 # python train.py -s /home/cglab/project/Dataset_opensource/Mip-NeRF360/garden -m results/Mip-NeRF360/garden/downsample_3.5 --downsampling 3.5 --test_hold 20 --lod_min 1 --lod_max 2 --increase_lod_num_childs 2
-python train_lod.py -s /home/cglab/project/Dataset_opensource/Mip-NeRF360/garden -m results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5 --downsampling 3.5 --test_hold 20 --lod_min 1 --lod_max 3 --increase_lod_num_childs 2
+python train_lod.py -s /home/cglab/project/Dataset_opensource/Mip-NeRF360/garden -m results/Mip-NeRF360/garden/changing_LoD_v1/downsample_3.5/diff_gaussian_rasterizer_SH_MIX_SG/ --downsampling 3.5 --test_hold 20 --lod_min 1 --lod_max 3 --increase_lod_num_childs 2
